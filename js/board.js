ec28350114fbccd42e030ac4b37264b93dc73850
MFauzanAP <muhammadfauzanaristyaputra@gmail.com>
2022-01-22 11:03:34 +0300

    Implement win validation function

--- js/board.js
+++ js/board.js
@@ -1,18 +1,171 @@
 //	Holds the current state of the board
 let board = [];
 
+//	Holds the moves made
+let moves = { X: [], O: [] };
+
+//	Hold current score
+let score = [];
+
+//	Holds current player
+let curPlayer = 'X';
+
+//	Hold the current round
+let round = 0;
+
 //	Function called to reset the board
 let ResetBoard = () => {
 
 	//	Create a new board
 	let temp = [
-		[ '', '', '' ],
-		[ '', '', '' ],
-		[ '', '', '' ],
+		[ '.', '.', '.' ],
+		[ '.', '.', '.' ],
+		[ '.', '.', '.' ],
 	]
+
+	//	Reset list of moves
+	moves = { X: [], O: [] };
+
+	//	Reset score
+	score = [ 0, 0 ];
 
 	//	Update board state
 	UpdateBoard(temp);
+
+}
+
+//	Function called to choose a box
+let ChooseBox = (x, y) => {
+
+	//	If this box is already selected then exit
+	if (board[x][y] !== '.') return
+
+	//	Add to list of moves
+	moves[curPlayer].push([ x, y ]);
+
+	//	Generate updated board
+	board[x][y] = curPlayer;
+
+	//	Update board state
+	UpdateBoard(board);
+
+	//	Check for a win
+	if (CheckWin()) EndGame(curPlayer);
+	else if (round == 8) EndGame('draw');
+
+	//	Switch current player
+	curPlayer = curPlayer == 'X' ? 'O' : 'X';
+
+	//	Increment round
+	round++;
+
+}
+
+//	Function called to check for a win
+let CheckWin = () => {
+
+	//	If each player still doesn't have 3 boxes yet then exit
+	if (round < 4) return false;
+
+	//	Go through each move made by the current player
+	for (let i = 0; i < moves[curPlayer].length; i++) {
+
+		//	Get the position at this index
+		let pos = moves[curPlayer][i];
+
+		//	Temporary counter variable
+		let j = 1;
+		let x = pos[0];
+		let y = pos[1];
+
+		//	Check for boxes north
+		while (x - j >= 0 && j <= 2) {
+
+			//	Get box
+			let box = x - j >= 0 ? board[x - j][y] : '';
+
+			//	If this box is empty or not the current players box then break the check
+			if (box !== curPlayer) break;
+
+			//	Else if the whole column is for the current player
+			else if (box === curPlayer && j == 2) return true;
+
+			//	Else then increment j
+			j++;
+
+		}
+
+		//	Reset j
+		j = 1;
+
+		//	Check for boxes north east
+		while (x - j >= 0 && y + j <= 2 && j <= 2) {
+
+			//	Get box
+			let box = x - j >= 0 && y + j <= 2 ? board[x - j][y + j] : '';
+
+			//	If this box is empty or not the current players box then break the check
+			if (box !== curPlayer) break;
+
+			//	Else if the whole diagonal is for the current player
+			else if (box === curPlayer && j == 2) return true;
+
+			//	Else then increment j
+			j++;
+
+		}
+
+		//	Reset j
+		j = 1;
+
+		//	Check for boxes east
+		while (y + j <= 2 && j <= 2) {
+
+			//	Get box
+			let box = y + j <= 2 ? board[x][y + j] : '';
+
+			//	If this box is empty or not the current players box then break the check
+			if (box !== curPlayer) break;
+
+			//	Else if the whole row is for the current player
+			else if (box === curPlayer && j == 2) return true;
+
+			//	Else then increment j
+			j++;
+
+		}
+
+		//	Reset j
+		j = 1;
+
+		//	Check for boxes south east
+		while (x + j <= 2 && y + j <= 2 && j <= 2) {
+
+			//	Get box
+			let box = x + j <= 2 && y + j <= 2 ? board[x + j][y + j] : '';
+
+			//	If this box is empty or not the current players box then break the check
+			if (box !== curPlayer) break;
+
+			//	Else if the whole diagonal is for the current player
+			else if (box === curPlayer && j == 2) return true;
+
+			//	Else then increment j
+			j++;
+
+		}
+
+	}
+
+	//	Return false
+	return false;
+
+}
+
+//	Function called when a player wins
+let EndGame = (winner) => {
+
+	console.log(winner);
 
 }
 
@@ -32,10 +185,10 @@ let UpdateBoard = (newBoard) => {
 		for (let y = 0; y < board[x].length; y++) {
 
 			//	Calculate index
-			let index = (y * 3) + x;
+			let index = (x * 3) + y;
 
-			//	Update box element at this position
-			boxes[index].textContent = board[x][y];
+			//	Update box element at this position if not a .
+			if (board[x][y] !== '.') boxes[index].textContent = board[x][y];
 
 		}
 
